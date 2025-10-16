e7e691437a96f382f236fd319e6868818b96740f
Diego Cardona <160233987+diegocardonahernandez@users.noreply.github.com>
2025-10-16 22:38:52 +0200

    Ejercicio provedores corregido

--- src/components/ServiceApiSuppliers.jsx
+++ src/components/ServiceApiSuppliers.jsx
@@ -1,70 +1,76 @@
-
-import React, { Component } from 'react'
 import axios from 'axios'
+import react from 'react'
+import React, { Component } from 'react'
 
 export default class ServiceApiSuppliers extends Component {
 
   url = "https://services.odata.org/V4/Northwind/Northwind.svc/Suppliers"
-  idBuscar = React.createRef()
+  cajaid = React.createRef()
 
   state = {
     suppliers: [],
-    supplier: {}
+    supplier: null
   }
 
-  loadSuppliers = () => {
-    console.log("Ingresando al servicio...")
-    axios.get(this.url).then(response => {
-      console.log("Leyendo servicio...")
+  loadSuppliers = () =>{
+
+    console.log("Cargando proveedores...")
+    axios.get(this.url).then(response=>{
+      console.log("Provedoores recibidos!")
       this.setState({
         suppliers: response.data.value
       })
     })
   }
 
-  loadInfoSupplier = () => {
-    console.log("Petición de busqueda de cliente recibida!")
-    var idcaja = this.idBuscar.current.value
-
-    this.state.suppliers.map((supp, index) => {
-      var idSupplier = parseInt(supp.SupplierID)
-      if (idcaja == idSupplier) {
+  loadSupplierData = (e) =>{
+    e.preventDefault( )
+    console.log("Buscando provedor...")
+    var id = this.cajaid.current.value
+    this.state.suppliers.map((currentSupplier,index)=>{
+      if(id == currentSupplier.SupplierID){
         this.setState({
-          supplier: supp
+          supplier: currentSupplier
         })
-      }else{
-        console.log("No se ha encontrado ningún sipplier con ese")
       }
     })
-    console.log(this.state.supplier)
+    
   }
 
-  //CORREGIR
 
-  componentDidMount = () => {
-    console.log("Cargando datos")
+  componentDidMount = () =>{
     this.loadSuppliers()
   }
-
 
   render() {
     return (
       <div>
-        <h1>
-          INFORMACIÓN CUSTOMERS
-        </h1>
-        <ul>
-          {
-            this.state.suppliers.map((supplier, index) => {
-              return (
-                <li key={index}>ID: <b>{supplier.SupplierID}</b> - Nombre: <b>{supplier.ContactName}</b></li>
-              )
-            })
-          }
-        </ul>
-        <label htmlFor="">Busque un cliente mediante su <b>ID: </b></label>
-        <input type="text" ref={this.idBuscar} />
-        <button onClick={this.loadInfoSupplier}>Buscar</button>
+        <h1>Service Supliers</h1>
+          <ul>
+              {
+                this.state.suppliers.map((currentSupplier,index)=>{
+                  return(
+                    <li key={index}><b>ID: </b>{currentSupplier.SupplierID} - <b>NOMBRE: </b>{currentSupplier.ContactName}</li>
+                  )
+                })
+              }
+          </ul>
+          <form onSubmit={this.loadSupplierData}>
+          <label htmlFor="">Indique un ID</label>
+          <input type="number" ref={this.cajaid}/>
+          <input type="submit" value="Buscar" />
+          </form>
+
+              {
+                (this.state.supplier == null )?
+                <h1 style={{color: "red"}}>No se ha ecnotrado ningúnn proveedor</h1>:
+                <div>
+                  <h2>Nombre: {this.state.supplier.ContactName}</h2>
+                  <h3>ID: {this.state.supplier.SupplierID}</h3>
+                  <h3>Compañia: {this.state.supplier.CompanyName}</h3>
+                </div>
+              }
+
       </div>
     )
   }
